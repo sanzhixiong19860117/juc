02fd34eaad46fdfde3a0445f5211a66f4d5aa023
sanzhixiong86 <315570835@qq.com>
2020-06-08 18:35:46 +0800

    增加了同步的操作

---
+++ day01/code/src/com/joy/home/Home2.java
@@ -0,0 +1,48 @@
+package com.joy.home;
+
+/**
+ * 模拟银行取款
+ */
+public class Home2 {
+    private String name;    //姓名
+    private double deposit; //存款
+
+    public synchronized void setValue(String name, double deposit) {
+        this.name = name;
+        this.deposit = deposit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public synchronized double getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(double deposit) {
+        this.deposit = deposit;
+    }
+
+    public static void main(String[] args) {
+        Home2 home2 = new Home2();
+
+        new Thread(()->home2.setValue("张三", 100.0));
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(home2.getDeposit());
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(home2.getDeposit());
+    }
+}
